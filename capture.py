93e761ef01b1c6f052ae72383743f4a29228c6a4
Ariel Steger <ariellekatherine92@gmail.com>
2022-06-22 15:49:40 -0400

    fixed timing

--- capture.py
+++ capture.py
@@ -2,16 +2,24 @@ import cv2, time
 
 video=cv2.VideoCapture(0)
 
-check, frame = video.read()
+a=0
 
-print(check)
-print(frame)
+while True:
+    a=a+1
+    check, frame = video.read()
 
-gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-time.sleep(3)
-cv2.imshow("Capturing", gray)
+    print(check)
+    print(frame)
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    
+    cv2.imshow("Capturing", gray)
 
 
-cv2.waitKey(0)
+    key=cv2.waitKey(1)
+
+    if key==ord('q'):
+        break
+
 video.release()
 cv2.destroyAllWindows()
