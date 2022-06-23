81ffbe90e6b80bd605c94a8aa9596286f36215b8
Ariel Steger <ariellekatherine92@gmail.com>
2022-06-23 13:58:06 -0400

    messing with frames

--- capture.py
+++ capture.py
@@ -1,22 +1,35 @@
 import cv2, time
 
+first_frame=None
+
 video=cv2.VideoCapture(0)
 
-a=0
+
 
 while True:
-    a=a+1
     check, frame = video.read()
 
-    print(check)
-    print(frame)
-
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray,(21,21),0)
+
+    if first_frame is None:
+        first_frame=gray
+        continue
+
+    delta_frame=cv2.absdiff(first_frame,gray)
+
+    thresh_delta=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+
+    thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+
     
     cv2.imshow("Capturing", gray)
+    cv2.imshow("Delta Frame", delta_frame)
+    cv2.imshow('Threshold Frame',thresh_delta)
 
 
     key=cv2.waitKey(1)
+    print(gray)
 
     if key==ord('q'):
         break
